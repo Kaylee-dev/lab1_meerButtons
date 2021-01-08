733d64a16bce3069eea05e520ce07ff00854fb7d
Kaylee <99057337@mydavinci.nl>
2021-01-08 10:50:51 +0100

    opdracht C af, opdracht D maken

--- buttons2D.js
+++ buttons2D.js
@@ -5,26 +5,27 @@ for(i = 1; i <= 30; i++){
 	document.body.appendChild(button);
 	var text = document.createTextNode(i);
 	button.appendChild(text);
-	button.style.backgroundColor = "green";
+	var color = ["green", "pink", "salmon", "violet", "black"]
+	button.style.backgroundColor = color[0];
 	if (i == 5 || i == 10 || i == 15 || i == 20 || i == 25) {
 		document.body.appendChild(document.createElement("br"));
 	}
 	button.onclick = function(){
 		var currentColor = this.style.backgroundColor;
-		if (currentColor == "green"){
-			this.style.backgroundColor = "red";
+		if (currentColor == color[0]){
+			this.style.backgroundColor = color[1];
 		}
-		else if (currentColor == "red") {
-			this.style.backgroundColor = "purple";
+		else if (currentColor == color[1]) {
+			this.style.backgroundColor = color[2];
 		}
-		else if (currentColor == "purple") {
-			this.style.backgroundColor = "blue";
+		else if (currentColor == color[2]) {
+			this.style.backgroundColor = color[3];
 		}
-		else if (currentColor == "blue") {
-			this.style.backgroundColor = "black";
+		else if (currentColor == color[3]) {
+			this.style.backgroundColor = color[4];
 			this.style.color = "white";
 		}
-		else if (currentColor == "black") {
+		else if (currentColor == color[4]) {
 			this.remove();
 		}
 }
